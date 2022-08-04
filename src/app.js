c1fb8548d554296f496b239422d735ecf618c9fb
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-08-05 00:54:31 +0300

    Updated the time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,17 @@
+function formatDate(timestamp) {
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours < 10) {
+        hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+    let day = days[date.getDay()];
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    return `${day} ${hours}: ${minutes}`;
+}
 function displayTemperature(response) {
     let temperatureElement = document.querySelector('#temperature');
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -9,6 +23,8 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     let windElement = document.querySelector('#wind');
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    let dateElement = document.querySelector('#date');
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 let apiKey = 'de2ebf2c340b35b80bea446a7df3ad64';
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${apiKey}`;
