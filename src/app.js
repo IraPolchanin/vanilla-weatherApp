315c8ac290a006fa1954f5df4528d34e0f0c81aa
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-08-11 00:58:30 +0300

    added API call

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
     let forecastElement = document.querySelector("#forecast");
     let forecastHTML = `<div class = "row">`;
     let days = ['Thu', 'Fri', 'Sat'];
@@ -41,6 +41,12 @@ function displayForecast() {
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+    let apiKey = 'de2ebf2c340b35b80bea446a7df3ad64';
+    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`;
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
     celsiusTemperature = response.data.main.temp;
     let temperatureElement = document.querySelector('#temperature');
@@ -58,7 +64,7 @@ function displayTemperature(response) {
     let iconElement = document.querySelector('#icon');
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
     iconElement.setAttribute("alt", response.data.weather[0].description);
-
+    getForecast(response.data.coord);
 }
 function search(city) {
     let apiKey = 'de2ebf2c340b35b80bea446a7df3ad64';
@@ -102,4 +108,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Kharkiv");
-displayForecast();
