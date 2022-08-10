c26df07fa40b8c8c6624884cda420b37bdd107bc
Iryna Polchaninova <ira.polchanin@gmail.com>
2022-08-10 18:50:53 +0300

    added JS template

--- src/app.js
+++ src/app.js
@@ -12,6 +12,35 @@ function formatDate(timestamp) {
     let day = days[date.getDay()];
     return `${day} ${hours}:${minutes}`;
 }
+
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHTML = `<div class = "row">`;
+    let days = ['Thu', 'Fri', 'Sat'];
+    days.forEach(function (day) {
+        forecastHTML = forecastHTML + `
+            <div class="col-2">
+                <div class="weather-forecast-date">
+                    ${day}
+                </div>
+                    <img src="http://openweathermap.org/img/wn/04d@2x.png" alt="" width="36" />
+            <div class="weather-forecast-temperature">
+                <span class="weather-forecast-temperature-max">
+                    18°
+                </span>
+                <span class="weather-forecast-temperature-min">
+                    12°
+                </span>
+            </div>
+            </div>
+        `;
+
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
     celsiusTemperature = response.data.main.temp;
     let temperatureElement = document.querySelector('#temperature');
@@ -71,3 +100,6 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+search("Kharkiv");
+displayForecast();
